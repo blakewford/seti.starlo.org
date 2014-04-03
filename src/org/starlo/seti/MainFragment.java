9885adac3865bb1c876204979aea6a33939f1687
Blake W. Ford <blakewford@gmail.com>
2014-04-02 22:52:53 -0500

    Get data from an async task

--- src/org/starlo/seti/MainFragment.java
+++ src/org/starlo/seti/MainFragment.java
@@ -1,21 +1,34 @@
 package org.starlo.seti;
 
 import android.app.ListFragment;
-import android.os.Bundle;
+import android.os.*;
 import android.view.*;
 import android.widget.*;
 
 public class MainFragment extends ListFragment {
-
-    private String[] mContacts = new String[] { "Blake W. Ford" };
 
     public MainFragment(){}
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState)
     {
-        setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, mContacts));
+        new AsyncBuilder().execute();
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    private class AsyncBuilder extends AsyncTask<Void, Void, String[]>
+    {
+
+         protected String[] doInBackground(Void... nothing)
+         {
+             return new String[] { "Blake W. Ford" };
+         }
+
+         protected void onPostExecute(String[] result)
+         {
+             setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, result));
+         }
+
     }
 
 };
