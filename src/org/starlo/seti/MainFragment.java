d48f61f7dfa2527c6826218861a4757d5093e26a
Blake W. Ford <blakewford@gmail.com>
2014-04-02 22:24:33 -0500

    Update fragment to list fragment

--- src/org/starlo/seti/MainFragment.java
+++ src/org/starlo/seti/MainFragment.java
@@ -1,16 +1,21 @@
 package org.starlo.seti;
 
-import android.app.Fragment;
+import android.app.ListFragment;
 import android.os.Bundle;
 import android.view.*;
+import android.widget.*;
 
-public class MainFragment extends Fragment {
+public class MainFragment extends ListFragment {
+
+    private String[] mContacts = new String[] { "Blake W. Ford" };
 
     public MainFragment(){}
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.contact_list, null);
+    public void onViewCreated(View view, Bundle savedInstanceState)
+    {
+        setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, mContacts));
+        super.onViewCreated(view, savedInstanceState);
     }
 
 };
