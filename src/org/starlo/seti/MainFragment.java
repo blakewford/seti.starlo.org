dfd26b040b43e1b608bd144f0dbbe2b771375d88
Blake W. Ford <blakewford@gmail.com>
2014-04-02 23:57:30 -0500

    Get accounts, setup dialog

--- src/org/starlo/seti/MainFragment.java
+++ src/org/starlo/seti/MainFragment.java
@@ -1,33 +1,77 @@
 package org.starlo.seti;
 
-import android.app.ListFragment;
+import android.app.*;
 import android.os.*;
 import android.view.*;
 import android.widget.*;
+import android.accounts.*;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import android.content.DialogInterface;
 
 public class MainFragment extends ListFragment {
 
+    private AccountManager mAccountManager = null;
+
     public MainFragment(){}
+
+    public void onAttach(Activity activity)
+    {
+        mAccountManager = AccountManager.get(activity);
+        super.onAttach(activity);
+    }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState)
     {
-        new AsyncBuilder().execute();
+        new AccountsDialogFragment().show(getActivity().getFragmentManager(), null);
         super.onViewCreated(view, savedInstanceState);
     }
 
     private class AsyncBuilder extends AsyncTask<Void, Void, String[]>
     {
 
-         protected String[] doInBackground(Void... nothing)
-         {
-             return new String[] { "Blake W. Ford" };
-         }
+        protected String[] doInBackground(Void... nothing)
+        {
+            return new String[]{};
+        }
 
-         protected void onPostExecute(String[] result)
-         {
-             setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, result));
-         }
+        protected void onPostExecute(String[] result)
+        {
+            setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, result));
+        }
+
+    }
+
+    private class AccountsDialogFragment extends DialogFragment
+    {
+
+        private String[] getAccountNames()
+        {
+            Account[] accounts = mAccountManager.getAccountsByType(GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE);
+            String[] names = new String[accounts.length];
+            for (int i = 0; i < names.length; i++)
+            {
+                names[i] = accounts[i].name;
+            }
+            return names;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState)
+        {
+            setRetainInstance(true);
+            return new AlertDialog.Builder(getActivity())
+            .setCancelable(false)
+            .setItems(getAccountNames(),
+                new DialogInterface.OnClickListener()
+                {
+                    public void onClick(DialogInterface dialog, int item)
+                    {
+                        new AsyncBuilder().execute();
+                    }
+                }
+            ).create();
+        }
 
     }
 
